9676007cc72924b318a7338df428d4da894920f1
shazaman23 <intechninja@gmail.com>
2017-11-18 16:48:51 -0700

    Set up the basic state of the About Me section.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,73 @@
   </div>
 
   <div class="row about-me">
-    <div class="">
+    <div class="w-100 d-flex flex-column">
+
+      <!-- MAIN TITLE -->
+      <h2 class="main-title text-center font-weight-bold">About Me</h2>
+
+      <!-- FAMILY -->
+      <div class="strip d-flex flex-row align-items-center">
+
+        <h4 class="flex-1 text-center">Family</h4>
+
+        <div class="flex-1 text-center">
+          <img src="{{ URL::asset('/img/icons/home.png') }}" alt="family icon">
+        </div>
+
+      </div>
+
+      <hr>
+
+      <!-- GAMING -->
+      <div class="strip d-flex flex-row-reverse align-items-center">
+
+        <h4 class="flex-1 text-center">Gaming</h4>
+
+        <div class="flex-1 text-center">
+          <img src="{{ URL::asset('/img/icons/gamepad.png') }}" alt="family icon">
+        </div>
+
+      </div>
+
+      <hr>
+
+      <!-- LEARNING -->
+      <div class="strip d-flex flex-row align-items-center">
+
+        <h4 class="flex-1 text-center">Learning</h4>
+
+        <div class="flex-1 text-center">
+          <img src="{{ URL::asset('/img/icons/books.png') }}" alt="family icon">
+        </div>
+
+      </div>
+
+      <hr>
+
+      <!-- MOVIES -->
+      <div class="strip d-flex flex-row-reverse align-items-center">
+
+        <h4 class="flex-1 text-center">Movies</h4>
+
+        <div class="flex-1 text-center">
+          <img src="{{ URL::asset('/img/icons/tv.png') }}" alt="family icon">
+        </div>
+
+      </div>
+
+      <hr>
+
+      <!-- ADVENTURE -->
+      <div class="strip d-flex flex-row align-items-center">
+
+        <h4 class="flex-1 text-center">Adventure</h4>
+
+        <div class="flex-1 text-center">
+          <img src="{{ URL::asset('/img/icons/hiker.png') }}" alt="family icon">
+        </div>
+
+      </div>
 
     </div>
   </div>
