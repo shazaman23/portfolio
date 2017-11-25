94e52f28284312a2c25c55445c8204f396b4f5fa
Jake Killpack <intechninja@gmail.com>
2017-11-24 17:10:52 -0700

    Added mobile pictures and finished styling for the majority of the experience detail pages.

--- resources/views/experiences/index.blade.php
+++ resources/views/experiences/index.blade.php
@@ -7,16 +7,18 @@
   <!-- MAIN TITLE -->
   <h2 class="main-title text-center font-weight-bold">{{ $experience->brand }} - {{ $experience->title }}</h2>
 
-  <div class="row">
+  <div class="row align-items-center">
 
-    <div class="offset-md-1 col-md-5 features">
-      <p>{{ $experience->problem }}</p>
-      <p>{{ $experience->description }}</p>
-      <p><a href="{{ $experience->url }}">Check it out!!</a></p>
+    <div class="offset-md-1 col-md-6 features">
+        <p>{{ $experience->problem }}</p>
+        <p>{{ $experience->description }}</p>
+        <p><a href="{{ $experience->url }}">Check it out!!</a></p>
     </div>
 
-    <div class="col-md-7 display">
-
+    <div class="col-md-5 display cellphone">
+      <div class="screen-demo"
+           style="background: url('/img/screenshots/mobile/{{ $experience->backgroundImage }}');">
+      </div>
     </div>
 
   </div>
