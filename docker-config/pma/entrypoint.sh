a7acbda8f7ad97da2deb91153aa23ac067e3f677
Jacob Killpack <intechninja@gmail.com>
2021-11-11 21:28:16 -0800

    Trying to update phpMyAdmin to use the right ssl certificate.

--- docker-config/pma/entrypoint.sh
+++ docker-config/pma/entrypoint.sh
@@ -7,9 +7,9 @@ if [ -d "/etc/letsencrypt/live" ]; then
 
   sed -ri -e 's,80,443,' /etc/apache2/sites-available/000-default.conf
   sed -i -e '/^<\/VirtualHost>/i SSLEngine on' /etc/apache2/sites-available/000-default.conf
-  sed -i -e '/^<\/VirtualHost>/i SSLCertificateFile /etc/letsencrypt/live/jakekillpack.com/cert.pem' /etc/apache2/sites-available/000-default.conf
-  sed -i -e '/^<\/VirtualHost>/i SSLCertificateKeyFile /etc/letsencrypt/live/jakekillpack.com/privkey.pem' /etc/apache2/sites-available/000-default.conf
-  sed -i -e '/^<\/VirtualHost>/i SSLCertificateChainFile /etc/letsencrypt/live/jakekillpack.com/fullchain.pem' /etc/apache2/sites-available/000-default.conf
+  sed -i -e '/^<\/VirtualHost>/i SSLCertificateFile /etc/letsencrypt/live/killfood.com/cert.pem' /etc/apache2/sites-available/000-default.conf
+  sed -i -e '/^<\/VirtualHost>/i SSLCertificateKeyFile /etc/letsencrypt/live/killfood.com/privkey.pem' /etc/apache2/sites-available/000-default.conf
+  sed -i -e '/^<\/VirtualHost>/i SSLCertificateChainFile /etc/letsencrypt/live/killfood.com/fullchain.pem' /etc/apache2/sites-available/000-default.conf
 fi
 
 exec "$@"
